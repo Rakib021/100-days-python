2b05d6caed83c36c516f9047c3aa4813eb46b9d3
Rakib <rakib@Rakibs-MacBook-Pro.local>
2023-05-25 16:04:09 +0600

    day-6

---
+++ calculator.py
@@ -0,0 +1,17 @@
+a = int(input())
+b = int(input())
+c=str(input())
+
+if c=="+":
+        print(a+b)
+elif c=="-":
+    print(a-b)
+elif c=="*":
+    print(a*b)
+elif c=="/":
+    print(a/b)
+elif c=="%":
+    print(a%b)
+else :
+    print("there has no operator\n")
+
